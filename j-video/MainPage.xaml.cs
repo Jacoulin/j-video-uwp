dee004c4349c0f1962d116db44102e3633cacb9a
Jacoulin <405563383@qq.com>
2018-09-11 23:39:54 +0800

    更改标题颜色

--- j-video/MainPage.xaml.cs
+++ j-video/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,27 @@ namespace j_video
         public MainPage()
         {
             this.InitializeComponent();
+
+            // using Windows.UI.ViewManagement;
+
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            // Set active window colors
+            titleBar.ForegroundColor = Colors.White;
+            titleBar.BackgroundColor = Color.FromArgb(1,186, 34, 38);
+            titleBar.ButtonForegroundColor = Colors.White;
+            titleBar.ButtonBackgroundColor = Color.FromArgb(1, 186, 34, 38);
+            titleBar.ButtonHoverForegroundColor = Colors.White;
+            titleBar.ButtonHoverBackgroundColor = Color.FromArgb(1, 186, 80, 80);
+            titleBar.ButtonPressedForegroundColor = Colors.Gray;
+            titleBar.ButtonPressedBackgroundColor = Color.FromArgb(1, 186, 50, 50);
+
+            // Set inactive window colors
+            titleBar.InactiveForegroundColor = Colors.Gray;
+            titleBar.InactiveBackgroundColor = Color.FromArgb(1, 186, 34, 38);
+            titleBar.ButtonInactiveForegroundColor = Colors.Gray;
+            titleBar.ButtonInactiveBackgroundColor = Color.FromArgb(1, 186, 34, 38);
+
         }
     }
 }
